5e338a4a22609fc34a751177bd1a29d9768ab2c6
kristina <kistina@localhost>
2021-06-22 19:21:25 +0300

    added not toBe

--- src/js/__tests__/test.metch.js
+++ src/js/__tests__/test.metch.js
@@ -8,3 +8,12 @@ test('сортировка от большего к меньшему', () => {
     { name: 'мечник', health: 10 },
   ]);
 });
+
+test('сортировка от большего к меньшему', () => {
+  const result = data(arrayHeroes);
+  expect(result).not.toBe([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
